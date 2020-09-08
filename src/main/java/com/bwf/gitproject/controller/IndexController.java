df27aacb5cceae76c583d34f9ce995ced4571996
titushan <titushan2010@163.com>
2020-09-08 23:15:52 +0800

    韩波 提交于 2020/09/08 23:15

---
+++ src/main/java/com/bwf/gitproject/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.bwf.gitproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping( value = { "/" , "/index" })
+public class IndexController {
+
+    @RequestMapping( value = { "/" , "/index" })
+    @ResponseBody
+    public String index(){
+
+        return "hello gitproject";
+    }
+}
